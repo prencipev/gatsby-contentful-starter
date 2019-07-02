1591b9eac5df8390dc7b498e9654778a94fe835e
Vincenzo Prencipe <v.prencipe@reply.it>
2019-07-02 17:06:03 +0200

    navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,24 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a className="navbar-brand" href="#">Carousel</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className={styles.navigationItem}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link to="/blog/">Blog</Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  </header>
 )
